c5554640df43ffc4b0013c8ed93afcf2172d49d3
Aletheia06 <Aletheia06@proton.me>
2026-09-23 21:33:38 +0800

    完成了struct1

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -9,9 +9,12 @@
 
 struct ColorClassicStruct {
     // TODO: Something goes here
+    red:i32,
+    green: i32,
+    blue:i32
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(/* TODO: Something goes here */i32, i32, i32);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
@@ -25,6 +28,12 @@ mod tests {
         // TODO: Instantiate a classic c struct!
         // let green =
 
+        let green = ColorClassicStruct{
+            red: 0,
+            green: 255,
+            blue: 0
+        };
+
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
         assert_eq!(green.blue, 0);
@@ -35,6 +44,7 @@ mod tests {
         // TODO: Instantiate a tuple struct!
         // let green =
 
+        let green = ColorTupleStruct(0, 255, 0);
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
         assert_eq!(green.2, 0);
@@ -44,6 +54,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
         // let unit_like_struct =
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
